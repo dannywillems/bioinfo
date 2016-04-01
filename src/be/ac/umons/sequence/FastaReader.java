2af67f5251b58dbf9731f5f7bf72129807699e41
Aline <goeminne.aline@gmail.com>
2016-04-01 13:04:46 +0200

    lecture/écriture de fichiers FASTA

---
+++ src/be/ac/umons/sequence/FastaReader.java
@@ -0,0 +1,47 @@
+package be.ac.umons.sequence;
+
+/**
+ * Created by aline on 31/03/16.
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * This class allows reading of a FASTA file.
+ */
+public class FastaReader
+{
+    /**
+     * Reads a text file and generates a sequence of DNA sequences based on this file,
+     * supposing the file use FASTA as data structure.
+     * @param f a FASTA file
+     * @return a sequence of DNA sequences read from a FASTA file.
+     */
+    public static List<Sequence> readFromFile(File f) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new FileReader(f));
+
+        StringBuilder builder = new StringBuilder();
+        List<Sequence> ret = new ArrayList<Sequence>();
+        String line;
+
+        while((line = br.readLine()) != null)
+        {
+            if(line.startsWith(">"))
+            {
+                String text = builder.toString();
+                if(!text.isEmpty())
+                    ret.add(new Sequence(text));
+
+                builder = new StringBuilder();
+            }
+            else
+                builder.append(line.trim());
+        }
+
+        return ret;
+    }
+}
