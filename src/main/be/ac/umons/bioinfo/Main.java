6fa1c2d8fe8f7d84a51c4285a062c5df51bb641e
Aline <goeminne.aline@gmail.com>
2016-04-08 19:14:04 +0200

    premier "test" de greedy dans le main

--- src/main/be/ac/umons/bioinfo/Main.java
+++ src/main/be/ac/umons/bioinfo/Main.java
@@ -4,15 +4,36 @@ package be.ac.umons.bioinfo;
  * Created by aline on 31/03/16.
  */
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import be.ac.umons.bioinfo.sequence.*;
 
 public class Main
 {
     public static void main(String[] args) throws IOException
     {
-        Sequence s = new Sequence("attagaccatgcggc");
-        Sequence t = new Sequence("atcggcattcagt");
+        Greedy greed = new Greedy();
 
-        Sequence.semiGlobalAlignment(s, t, 1, -1, -2);
+        Sequence f = new Sequence("catagtc");
+        Sequence g = new Sequence("taactat");
+        Sequence h = new Sequence("agactatcc");
+
+        List<Sequence> list = new ArrayList<>();
+        list.add(f);
+        list.add(g);
+        list.add(h);
+
+        List<SequenceAlignment> result = greed.computePath(list, 1, -1, -2);
+        Iterator<SequenceAlignment> iterator = result.iterator();
+
+        while(iterator.hasNext())
+        {
+            SequenceAlignment alignment = iterator.next();
+            System.out.println(alignment.s1);
+            System.out.println(alignment.s2);
+            System.out.println("----------------");
+        }
     }
 }
