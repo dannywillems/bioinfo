057d4f257348700964021d89d879394ae7eb698a
Danny Willems <contact@danny-willems.be>
2016-04-10 22:23:30 +0200

    Arc.java: description des attributs

--- src/main/be/ac/umons/bioinfo/sequence/Arc.java
+++ src/main/be/ac/umons/bioinfo/sequence/Arc.java
@@ -3,15 +3,38 @@ package be.ac.umons.bioinfo.sequence;
 
 public class Arc
 {
+    /**
+     * The initial source sequence
+     */
     public final Sequence s1;
+    /**
+     * True if we need to use the complementary of s1
+     */
     public final boolean s1Comp;
 
+    /**
+     * The initial target sequence
+     */
     public final Sequence s2;
+    /**
+     * True if we need to use the complementary of s2
+     */
     public final boolean s2Comp;
 
+    /**
+     * The s1 sequence when aligned with s2. Can be the complementary depending
+     * on the value of s1Comp
+     */
     public final Sequence s1Aligned;
+    /**
+     * The s2 sequence when aligned with s1. Can be the complementary depending
+     * on the value of s2Comp
+     */
     public final Sequence s2Aligned;
 
+    /**
+     * The alignment score
+     */
     public final int score;
 
     public Arc(Sequence s1,
