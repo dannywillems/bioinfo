85a1580f8825e27980fa428a30b2309ae31a19a2
Danny Willems <contact@danny-willems.be>
2016-04-22 22:01:38 +0200

    Simplification de la fonction compare pour les arcs. Ce qui est important dans le compare c'est que ca soit positif, negatif ou nul en fonction de la comparaison

--- src/main/be/ac/umons/bioinfo/sequence/ArcComparator.java
+++ src/main/be/ac/umons/bioinfo/sequence/ArcComparator.java
@@ -11,9 +11,6 @@ public class ArcComparator implements Comparator<Arc>
     @Override
     public int compare(Arc a1, Arc a2)
     {
-        if (a1.score > a2.score)
-            return -1;
-        else
-            return (a1.score < a2.score ? 1 : 0);
+        return (a2.score - a1.score);
     }
 }
