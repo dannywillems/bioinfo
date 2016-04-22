69ad51e25817d461d30d0f28dc5d01b2c9c8d2a7
Aline <goeminne.aline@gmail.com>
2016-04-22 21:33:45 +0200

    modification des noms des attributs d'un arc

--- src/main/be/ac/umons/bioinfo/sequence/Arc.java
+++ src/main/be/ac/umons/bioinfo/sequence/Arc.java
@@ -1,31 +1,41 @@
 package be.ac.umons.bioinfo.sequence;
 
 
-public class Arc
-{
+public class Arc {
 
-    public final Sequence s1; // The initial source sequence
-    public final boolean s1Comp; //True if we need to use the complementary of s1
-    public final Sequence s2; // The initial target sequence
-    public final boolean s2Comp; //True if we need to use the complementary of s2
-    public final Sequence s1Aligned; // The s1 sequence when aligned with s2. Can be the complementary.
-    public final Sequence s2Aligned; //he s2 sequence when aligned with s1. Can be the complementary
+    public final Sequence start; // The initial source sequence
+    public final boolean startComp; //True if we need to use the complementary of start
+    public final Sequence end; // The initial target sequence
+    public final boolean endComp; //True if we need to use the complementary of end
+    public final Sequence alignedStart; // The start sequence when aligned with end. Can be the complementary.
+    public final Sequence alignedEnd; //he end sequence when aligned with start. Can be the complementary
     public final int score; //The alignment score
 
-    public Arc(Sequence s1,
-               boolean s1Comp,
-               Sequence s2,
-               boolean s2Comp,
-               Sequence s1Aligned,
-               Sequence s2Aligned,
-               int score)
-    {
-        this.s1 = s1;
-        this.s1Comp = s1Comp;
-        this.s2 = s2;
-        this.s2Comp = s2Comp;
-        this.s1Aligned = s1Aligned;
-        this.s2Aligned = s2Aligned;
+    /**
+     * A representation of a transition between two sequences.
+     *
+     * @param start        The canonical representation of the sequence at the origin of the arc.
+     * @param startComp    true if the complement of start is used instead of the sequence itself; false otherwise.
+     * @param end          The canonical representation of the sequence at the end of the arc.
+     * @param endComp      true if the complement of end is used instead of the sequence itself; false otherwise.
+     * @param alignedStart the actually used start sequence after its alignment with the actual end.
+     * @param alignedEnd   the actually used end sequence after its alignement with the actual start.
+     * @param score        length of the longest suffix-prefix or prefix-suffix.
+     */
+    public Arc(Sequence start,
+               boolean startComp,
+               Sequence end,
+               boolean endComp,
+               Sequence alignedStart,
+               Sequence alignedEnd,
+               int score) {
+        this.start = start;
+        this.startComp = startComp;
+        this.end = end;
+        this.endComp = endComp;
+        this.alignedStart = alignedStart;
+        this.alignedEnd = alignedEnd;
         this.score = score;
     }
+
 }
