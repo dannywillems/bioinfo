aff28ca105c76b046a851540ddc72cfc6cceb84c
Danny Willems <contact@danny-willems.be>
2016-04-27 16:56:43 +0200

    Consensus: reste l'alignement à faire en ajoutant les gaps. Le calcul de la plus grande occurences est faite en supposant que chaque sequence est de même longueur

--- src/main/be/ac/umons/bioinfo/sequence/Consensus.java
+++ src/main/be/ac/umons/bioinfo/sequence/Consensus.java
@@ -1,18 +1,73 @@
 package be.ac.umons.bioinfo.sequence;
 
 import java.util.List;
+import java.lang.Integer;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class Consensus
 {
     private List<SequenceAlignment> hamiltonian_path;
+    private List<Sequence> alignment;
 
     public Consensus(List<SequenceAlignment> hamiltonian_path)
     {
         this.hamiltonian_path = hamiltonian_path;
     }
 
-    public List<SequenceAlignment> addGap()
+    /**
+     * Do the alignment and save it in alignment attribute.
+     */
+    public void alignment()
     {
-        return (this.hamiltonian_path);
+    }
+
+    /**
+     * Build the consensus based on the alignment done in the alignment method.
+     * Use the alignment attribute.
+     * @return the consensus as a Sequence object
+     */
+    public Sequence build()
+    {
+        StringBuilder consensus = new StringBuilder();
+        int size_consensus = this.alignment.get(0).getSize();
+        for (int i = 0;i < size_consensus;i++)
+        {
+            HashMap<Character, Integer> occurences = new HashMap<Character, Integer>();
+            for (int j = 0;j < alignment.size();j++)
+            {
+                Character c = new Character(alignment.get(j).getLetter(i));
+                Integer c_occurence = occurences.get(c);
+                if (c_occurence.equals(new Integer(0)))
+                    occurences.put(c, new Integer(0));
+                else
+                    occurences.put(c, new Integer(c_occurence.intValue() + Integer.valueOf(1)));
+            }
+            consensus.append(this.getBase(occurences));
+        }
+        return (new Sequence(consensus.toString()));
+    }
+
+    /**
+     * Return the more occured character at a position.
+     * If equality, it takes the first.
+     */
+    public char getBase(HashMap<Character, Integer> o)
+    {
+        int max = Integer.MIN_VALUE;
+        char c_max = '-';
+
+        Iterator<Character> iterator_o = o.keySet().iterator();
+        while (iterator_o.hasNext())
+        {
+            Character c = iterator_o.next();
+            if (o.get(c).intValue() > max)
+            {
+                max = o.get(c).intValue();
+                c_max = c.charValue();
+            }
+        }
+        return (c_max);
     }
 }
---
+++ src/test/be/ac/umons/bioinfo/sequence/ConsensusTest.java
@@ -0,0 +1,38 @@
+package be.ac.umons.bioinfo.sequence;
+
+import org.junit.Ignore;
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+import java.util.HashMap;
+import java.lang.Character;
+import java.lang.Integer;
+
+public class ConsensusTest
+{
+    @Test
+    public void getBaseTest()
+    {
+        Consensus c = new Consensus(null);
+
+        HashMap<Character, Integer> s = new HashMap<Character, Integer>();
+        s.put(new Character('c'), 5);
+        s.put(new Character('g'), 2);
+        s.put(new Character('t'), 4);
+        s.put(new Character('-'), 1);
+        assertEquals(c.getBase(s), 'c');
+    }
+
+    @Test
+    public void getBaseMultipleTest()
+    {
+        Consensus c = new Consensus(null);
+
+        HashMap<Character, Integer> s = new HashMap<Character, Integer>();
+        s.put(new Character('c'), 5);
+        s.put(new Character('g'), 5);
+        s.put(new Character('t'), 4);
+        s.put(new Character('-'), 1);
+        assertEquals(c.getBase(s), 'c');
+    }
+}
