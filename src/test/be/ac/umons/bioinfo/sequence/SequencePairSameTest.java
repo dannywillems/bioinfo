2ef489a04188b095c398bf81dc21108d05857ec9
Danny Willems <contact@danny-willems.be>
2016-05-03 10:07:01 +0200

    SequencePairSame: ajout de test

--- src/test/be/ac/umons/bioinfo/sequence/SequencePairSameTest.java
+++ src/test/be/ac/umons/bioinfo/sequence/SequencePairSameTest.java
@@ -49,4 +49,56 @@ public class SequencePairSameTest
         assertEquals(new Sequence("c-gat--c-tg"), a.s);
         assertEquals(new Sequence("c-gat--c-tg"), a.t);
     }
+
+    @Test
+    public void rebuildWithGapsBeginTest()
+    {
+        Sequence initial = new Sequence("cgatctg");
+        Sequence s = new Sequence("--cgat-c-tg");
+        Sequence t = new Sequence("c-gat--ctg");
+        SequencePairSame a = new SequencePairSame(initial, s, t);
+
+        a.rebuildWithGaps();
+        assertEquals(new Sequence("--c-gat--c-tg"), a.s);
+        assertEquals(new Sequence("--c-gat--c-tg"), a.t);
+    }
+
+    @Test
+    public void rebuildWithGapsBeginOrderDoesntChangeTest()
+    {
+        Sequence initial = new Sequence("cgatctg");
+        Sequence s = new Sequence("--cgat-c-tg");
+        Sequence t = new Sequence("c-gat--ctg");
+        SequencePairSame a = new SequencePairSame(initial, t, s);
+
+        a.rebuildWithGaps();
+        assertEquals(new Sequence("--c-gat--c-tg"), a.s);
+        assertEquals(new Sequence("--c-gat--c-tg"), a.t);
+    }
+
+    @Test
+    public void rebuildWithGapsBeginBothTest()
+    {
+        Sequence initial = new Sequence("cgatctg");
+        Sequence s = new Sequence("--cgat-c-tg");
+        Sequence t = new Sequence("--c-gat--ctg");
+        SequencePairSame a = new SequencePairSame(initial, s, t);
+
+        a.rebuildWithGaps();
+        assertEquals(new Sequence("--c-gat--c-tg"), a.s);
+        assertEquals(new Sequence("--c-gat--c-tg"), a.t);
+    }
+
+    @Test
+    public void rebuildWithGapsBeginBothOrderDoesntChangeTest()
+    {
+        Sequence initial = new Sequence("cgatctg");
+        Sequence s = new Sequence("--cgat-c-tg");
+        Sequence t = new Sequence("--c-gat--ctg");
+        SequencePairSame a = new SequencePairSame(initial, t, s);
+
+        a.rebuildWithGaps();
+        assertEquals(new Sequence("--c-gat--c-tg"), a.s);
+        assertEquals(new Sequence("--c-gat--c-tg"), a.t);
+    }
 }
