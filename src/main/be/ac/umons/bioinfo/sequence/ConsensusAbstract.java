1c6e6eae82a0c716f4ba75104be2ee70505b2e51
Danny Willems <contact@danny-willems.be>
2016-05-04 21:01:56 +0200

    ConsensusAbstract: getter et setters

--- src/main/be/ac/umons/bioinfo/sequence/ConsensusAbstract.java
+++ src/main/be/ac/umons/bioinfo/sequence/ConsensusAbstract.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 
 public class ConsensusAbstract
 {
-    public ArrayList<SequenceAlignmentAbstract> hamiltonian_path;
-    public ArrayList<SequenceAbstract> alignment;
+    private ArrayList<SequenceAlignmentAbstract> hamiltonian_path;
+    private ArrayList<SequenceAbstract> alignment;
 
+    /* ---------------------------------------------------------------------- */
+    // CONSTRUCTORS
     public ConsensusAbstract(List<SequenceAlignment> hamiltonian_path)
     {
         this.hamiltonian_path = new ArrayList<SequenceAlignmentAbstract>();
@@ -20,4 +22,38 @@ public class ConsensusAbstract
     {
         this.hamiltonian_path = hp;
     }
+    /* ---------------------------------------------------------------------- */
+
+    /* ---------------------------------------------------------------------- */
+    public void computeAlignment()
+    {
+
+    }
+    /* ---------------------------------------------------------------------- */
+
+    /* ---------------------------------------------------------------------- */
+    // GETTERS
+    public ArrayList<SequenceAbstract> getAlignment()
+    {
+        return (this.alignment);
+    }
+
+    public ArrayList<SequenceAlignmentAbstract> getHamiltonianPath()
+    {
+        return (this.hamiltonian_path);
+    }
+    /* ---------------------------------------------------------------------- */
+
+    /* ---------------------------------------------------------------------- */
+    // SETTERS
+    public void setAlignment(ArrayList<SequenceAbstract> a)
+    {
+        this.alignment = a;
+    }
+
+    public void setHamiltonianPath(ArrayList<SequenceAlignmentAbstract> hp)
+    {
+        this.hamiltonian_path = hp;
+    }
+    /* ---------------------------------------------------------------------- */
 }
