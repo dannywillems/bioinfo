96ecac92c221e4384f636e7e69f0c73c7683a3cf
Danny Willems <contact@danny-willems.be>
2016-05-05 23:54:27 +0200

    SequenceAbstract: addGapsAndReturnIndice --> méthode permettant de récupérer l'indice (dans la chaine initiale) du nucléotide après lequel on a ajouté les gaps. Sera utile pour le consensus

--- src/main/be/ac/umons/bioinfo/sequence/SequenceAbstract.java
+++ src/main/be/ac/umons/bioinfo/sequence/SequenceAbstract.java
@@ -104,7 +104,8 @@ public class SequenceAbstract //implements Iterable<Character>
      *  - addGaps(3, 0) on the sequence
      *      at--tgc ==> ---at--tgc
      * @param nb number of gaps to add
-     * @param pos where the gaps must be added
+     * @param pos where the gaps must be added. Absolute position eg including
+     * the offset.
      */
     public void addGaps(int nb, int pos)
     {
@@ -142,6 +143,70 @@ public class SequenceAbstract //implements Iterable<Character>
             }
             if (i == this.nb_gaps.length)
                 this.nb_gaps[i - 1] += nb;
+        }
+    }
+
+    /**
+     * Add [nb] gaps before the element at position [pos] and return the indice
+     * of the nucleotide which gaps has been inserted after, -1 if it's in the
+     * offset. If position is greater than the number of element in the
+     * sequence, gaps are added at the end, ie in the nb_gaps[sequence_size -
+     * 1].
+     * If position is before the offset or at 0, gaps are added as offset.
+     * Examples:
+     *  - addGaps(1, 1) on the sequence
+     *      attgc ==> a-ttgc and returns 0
+     *  - addGaps(1, 2) on the sequence
+     *      at--tgc ==> at---tgc and returns 1
+     *  - addGaps(3, 5) on the sequence
+     *      at--tgc ==> at--t---gc and returns 2
+     *  - addGaps(3, 0) on the sequence
+     *      at--tgc ==> ---at--tgc and returns -1
+     * @param nb number of gaps to add
+     * @param pos where the gaps must be added. Absolute position eg including
+     * the offset.
+     * @return indice of the nucleotide which gaps has been inserted after. -1
+     * if it's in the offset.
+     */
+    public int addGapsAndReturnIndice(int nb, int pos)
+    {
+        int i = 0;
+        int real_pos = this.offset;
+        // if we need to add gaps before the beginning of the sequence
+        if (pos <= real_pos)
+        {
+            this.offset += nb;
+            return (-1);
+        }
+        else // else, we looking for the position where we must add
+        {
+            while (i < this.nb_gaps.length)
+            {
+                // if the current real position (which is a nucleotide, not a
+                // gap) is exactly where we must add the gaps, we must add gaps
+                // before this nucleotide, so add gaps at the position i - 1.
+                if (real_pos == pos)
+                {
+                    this.nb_gaps[i - 1] += nb;
+                    return (i - 1);
+                }
+                // else if the next real position (ie real_pos + nb_gaps[i] + 1)
+                // is before where we need to add gaps, we go to the next
+                // position.
+                else if (real_pos + this.nb_gaps[i] < pos)
+                {
+                    real_pos += this.nb_gaps[i] + 1;
+                    i++;
+                }
+                // else, we add the number of gaps we need.
+                else
+                {
+                    this.nb_gaps[i] += nb;
+                    return (i);
+                }
+            }
+            this.nb_gaps[i - 1] += nb;
+            return (i - 1);
         }
     }
     /* ---------------------------------------------------------------------- */
