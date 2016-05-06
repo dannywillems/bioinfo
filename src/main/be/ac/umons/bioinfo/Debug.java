fff150a6275379a4112c5a100faa7fa90f92b568
Danny Willems <contact@danny-willems.be>
2016-05-06 20:53:18 +0200

    Debug: ajout d'un counter

--- src/main/be/ac/umons/bioinfo/Debug.java
+++ src/main/be/ac/umons/bioinfo/Debug.java
@@ -3,8 +3,10 @@ package be.ac.umons.bioinfo;
 public class Debug
 {
     public static boolean GAP_IN_CONSENSUS = false;
-    public static boolean OFFSET = true;
-    public static boolean EQUALITY_SAME_GAPS_NUMBER = true;
+    public static boolean OFFSET = false;
+    public static boolean EQUALITY_SAME_GAPS_NUMBER = false;
     public static boolean EQUALITY_SAME_GAPS_NUMBER_DURING_PROPAGATION = false;
     public static boolean PROPAGATION_GAP_DOWN_FROM = true;
+
+    public static int count_writer = 0;
 }
