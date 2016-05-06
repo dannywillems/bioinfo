5f8ed7cc8420c56b3038aa156ecaae29f372afaf
Danny Willems <contact@danny-willems.be>
2016-05-06 11:59:21 +0200

    Debug: ajout de variables pour débug

--- src/main/be/ac/umons/bioinfo/Debug.java
+++ src/main/be/ac/umons/bioinfo/Debug.java
@@ -3,8 +3,8 @@ package be.ac.umons.bioinfo;
 public class Debug
 {
     public static boolean GAP_IN_CONSENSUS = false;
-    public static boolean OFFSET = false;
-    public static boolean EQUALITY_SAME_GAPS_NUMBER = false;
+    public static boolean OFFSET = true;
+    public static boolean EQUALITY_SAME_GAPS_NUMBER = true;
     public static boolean EQUALITY_SAME_GAPS_NUMBER_DURING_PROPAGATION = false;
-    public static boolean PROPAGATION_GAP_DOWN_FROM = false;
+    public static boolean PROPAGATION_GAP_DOWN_FROM = true;
 }
